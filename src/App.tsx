58d8d4b21812568d48e638d9b2a5d4f33aca5c0c
Mahmud Hasan <mahmudhassan11752@gmail.com>
2024-01-18 22:47:12 +0600

    test animation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="box-1"></div>
+      <h1>This is a header</h1>
+    </div>
+  );
+}
+
+export default App;
